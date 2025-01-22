d2e7aa544a37b944af46e73ba701d8cc70f3b5a3
Will Woodlief <willwoodlief@gmail.com>
2025-01-22 09:33:32 -0600

    Minor change for new theme (no version)

--- resources/views/content-for-filters/invoice-attribute-cell.blade.php
+++ resources/views/content-for-filters/invoice-attribute-cell.blade.php
@@ -5,7 +5,7 @@ use \Scm\PluginSampleTheming\Models\ScmPluginSampleInventory;
      */
 @endphp
 <div class="p-1 text-center" style="background-color: {{$setting->invoice_color_code}}">
-    <span class="badge @if($setting->invoice_importance === ScmPluginSampleInventory::STATUS_NORMAL) badge-info @elseif($setting->invoice_importance === ScmPluginSampleInventory::STATUS_LEVEL_2) badge-warning @else badge-default @endif">
+    <span class="badge @if($setting->invoice_importance === ScmPluginSampleInventory::STATUS_NORMAL) text-bg-info @elseif($setting->invoice_importance === ScmPluginSampleInventory::STATUS_LEVEL_2) badge-warning @else badge-default @endif">
         {{md5($setting->invoice_importance)}}
     </span>
 </div>
